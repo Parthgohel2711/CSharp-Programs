f5d7163aa12d2a91843cd9cfcfee93861133ee1e
Parth Gohel <gohelparth444@gmail.com>
2025-04-11 16:19:57 +0530

    Ok

---
+++ Crud&Navigation/Crud&Navigation/Navigate.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Crud_Navigation
+{
+    partial class Navigate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Unm = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Cou = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BtnInsert = new System.Windows.Forms.Button();
+            this.BtnUpdate = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.UserData = new System.Windows.Forms.DataGridView();
+            this.BtnLast = new System.Windows.Forms.Button();
+            this.BtnPrev = new System.Windows.Forms.Button();
+            this.BtnNext = new System.Windows.Forms.Button();
+            this.BtnFirst = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.UserData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 103);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UserName";
+            // 
+            // Unm
+            // 
+            this.Unm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Unm.Location = new System.Drawing.Point(172, 95);
+            this.Unm.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Unm.Multiline = true;
+            this.Unm.Name = "Unm";
+            this.Unm.Size = new System.Drawing.Size(262, 33);
+            this.Unm.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 146);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Course";
+            // 
+            // Cou
+            // 
+            this.Cou.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Cou.FormattingEnabled = true;
+            this.Cou.Location = new System.Drawing.Point(172, 138);
+            this.Cou.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Cou.Name = "Cou";
+            this.Cou.Size = new System.Drawing.Size(262, 33);
+            this.Cou.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Century Gothic", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(47, 52);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(387, 30);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Enter User Details";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BtnInsert
+            // 
+            this.BtnInsert.BackColor = System.Drawing.Color.RoyalBlue;
+            this.BtnInsert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnInsert.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnInsert.Location = new System.Drawing.Point(47, 186);
+            this.BtnInsert.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.BtnInsert.Name = "BtnInsert";
+            this.BtnInsert.Size = new System.Drawing.Size(79, 38);
+            this.BtnInsert.TabIndex = 5;
+            this.BtnInsert.Text = "&Insert";
+            this.BtnInsert.UseVisualStyleBackColor = false;
+            this.BtnInsert.Click += new System.EventHandler(this.BtnInsert_Click);
+            // 
+            // BtnUpdate
+            // 
+            this.BtnUpdate.BackColor = System.Drawing.Color.Gray;
+            this.BtnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnUpdate.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnUpdate.Location = new System.Drawing.Point(138, 186);
+            this.BtnUpdate.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.BtnUpdate.Name = "BtnUpdate";
+            this.BtnUpdate.Size = new System.Drawing.Size(103, 38);
+            this.BtnUpdate.TabIndex = 6;
+            this.BtnUpdate.Text = "&Update";
+            this.BtnUpdate.UseVisualStyleBackColor = false;
+            this.BtnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.BackColor = System.Drawing.Color.Gray;
+            this.BtnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnDelete.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDelete.Location = new System.Drawing.Point(253, 186);
+            this.BtnDelete.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(90, 38);
+            this.BtnDelete.TabIndex = 7;
+            this.BtnDelete.Text = "&Delete";
+            this.BtnDelete.UseVisualStyleBackColor = false;
+            this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.BackColor = System.Drawing.Color.RoyalBlue;
+            this.BtnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnClear.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnClear.Location = new System.Drawing.Point(355, 186);
+            this.BtnClear.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(79, 38);
+            this.BtnClear.TabIndex = 8;
+            this.BtnClear.Text = "&Clear";
+            this.BtnClear.UseVisualStyleBackColor = false;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // UserData
+            // 
+            this.UserData.AllowUserToAddRows = false;
+            this.UserData.AllowUserToDeleteRows = false;
+            this.UserData.AllowUserToOrderColumns = true;
+            this.UserData.AllowUserToResizeColumns = false;
+            this.UserData.AllowUserToResizeRows = false;
+            this.UserData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.UserData.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.UserData.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.UserData.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.UserData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.RoyalBlue;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.UserData.DefaultCellStyle = dataGridViewCellStyle5;
+            this.UserData.GridColor = System.Drawing.Color.Black;
+            this.UserData.Location = new System.Drawing.Point(47, 275);
+            this.UserData.Name = "UserData";
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.UserData.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.UserData.Size = new System.Drawing.Size(387, 174);
+            this.UserData.TabIndex = 9;
+            // 
+            // BtnLast
+            // 
+            this.BtnLast.BackColor = System.Drawing.Color.RoyalBlue;
+            this.BtnLast.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLast.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLast.Location = new System.Drawing.Point(355, 457);
+            this.BtnLast.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.BtnLast.Name = "BtnLast";
+            this.BtnLast.Size = new System.Drawing.Size(79, 38);
+            this.BtnLast.TabIndex = 13;
+            this.BtnLast.Text = "&Last";
+            this.BtnLast.UseVisualStyleBackColor = false;
+            this.BtnLast.Click += new System.EventHandler(this.BtnLast_Click);
+            // 
+            // BtnPrev
+            // 
+            this.BtnPrev.BackColor = System.Drawing.Color.RoyalBlue;
+            this.BtnPrev.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnPrev.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPrev.Location = new System.Drawing.Point(253, 457);
+            this.BtnPrev.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.BtnPrev.Name = "BtnPrev";
+            this.BtnPrev.Size = new System.Drawing.Size(90, 38);
+            this.BtnPrev.TabIndex = 12;
+            this.BtnPrev.Text = "&Prev";
+            this.BtnPrev.UseVisualStyleBackColor = false;
+            this.BtnPrev.Click += new System.EventHandler(this.BtnPrev_Click);
+            // 
+            // BtnNext
+            // 
+            this.BtnNext.BackColor = System.Drawing.Color.RoyalBlue;
+            this.BtnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnNext.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnNext.Location = new System.Drawing.Point(138, 457);
+            this.BtnNext.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.BtnNext.Name = "BtnNext";
+            this.BtnNext.Size = new System.Drawing.Size(103, 38);
+            this.BtnNext.TabIndex = 11;
+            this.BtnNext.Text = "&Next";
+            this.BtnNext.UseVisualStyleBackColor = false;
+            this.BtnNext.Click += new System.EventHandler(this.BtnNext_Click);
+            // 
+            // BtnFirst
+            // 
+            this.BtnFirst.BackColor = System.Drawing.Color.RoyalBlue;
+            this.BtnFirst.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnFirst.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnFirst.Location = new System.Drawing.Point(47, 457);
+            this.BtnFirst.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.BtnFirst.Name = "BtnFirst";
+            this.BtnFirst.Size = new System.Drawing.Size(79, 38);
+            this.BtnFirst.TabIndex = 10;
+            this.BtnFirst.Text = "&First";
+            this.BtnFirst.UseVisualStyleBackColor = false;
+            this.BtnFirst.Click += new System.EventHandler(this.BtnFirst_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(47, 234);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(387, 30);
+            this.textBox1.TabIndex = 14;
+            this.textBox1.Text = "User Details";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Navigate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(482, 544);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.BtnLast);
+            this.Controls.Add(this.BtnPrev);
+            this.Controls.Add(this.BtnNext);
+            this.Controls.Add(this.BtnFirst);
+            this.Controls.Add(this.UserData);
+            this.Controls.Add(this.BtnClear);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.BtnUpdate);
+            this.Controls.Add(this.BtnInsert);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Cou);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Unm);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.Name = "Navigate";
+            this.Load += new System.EventHandler(this.Navigate_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.UserData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Unm;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox Cou;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button BtnInsert;
+        private System.Windows.Forms.Button BtnUpdate;
+        private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.DataGridView UserData;
+        private System.Windows.Forms.Button BtnLast;
+        private System.Windows.Forms.Button BtnPrev;
+        private System.Windows.Forms.Button BtnNext;
+        private System.Windows.Forms.Button BtnFirst;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Crud&Navigation/Crud&Navigation/Navigate.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crud_Navigation
+{
+    public partial class Navigate: Form
+    {
+        SqlConnection Con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Crud&Navigate.mdf;Integrated Security=True");
+        int Rp = 0, Tr = 0, id = 0;
+        
+        public Navigate()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadData()
+        {
+            String SelectQuery = "Select * From Course";
+            SqlDataAdapter DA = new SqlDataAdapter(SelectQuery,Con);
+            DataTable DT = new DataTable();
+            DA.Fill(DT);
+            if(DT.Rows.Count > 0)
+            {
+                Cou.DataSource = DT;
+            }
+            Cou.DisplayMember = "Cou1";
+            Cou.Text = "--Select Course--";
+
+            String SelectQuery1 = "Select * From UserDetails";
+            SqlDataAdapter DA1 = new SqlDataAdapter(SelectQuery1, Con);
+            DataTable DT1 = new DataTable();
+            DA1.Fill(DT1);
+            UserData.DataSource = DT1;
+            Tr = DT1.Rows.Count - 1;
+        }
+
+        private void Clear()
+        {
+            Unm.Text = String.Empty;
+            Cou.Text = "--Select Course--";
+            Unm.Focus();
+        }
+
+        private void Navigation()
+        {
+            String SelectQuery1 = "Select * From UserDetails";
+            SqlDataAdapter DA1 = new SqlDataAdapter(SelectQuery1, Con);
+            DataTable DT1 = new DataTable();
+            DA1.Fill(DT1);
+            id = Convert.ToInt32(DT1.Rows[Rp][0].ToString());
+            Unm.Text = DT1.Rows[Rp][1].ToString();
+            Cou.Text = DT1.Rows[Rp][2].ToString();
+        }
+
+        private void Enable()
+        {
+            BtnUpdate.Enabled = true;
+            BtnUpdate.BackColor = Color.RoyalBlue;
+            BtnDelete.Enabled = true;
+            BtnDelete.BackColor = Color.RoyalBlue;
+            BtnInsert.Enabled = false;
+            BtnInsert.BackColor = Color.Gray;
+        }
+
+        private void Disable()
+        {
+            BtnUpdate.Enabled = false;
+            BtnUpdate.BackColor = Color.Gray;
+            BtnDelete.Enabled = false;
+            BtnDelete.BackColor = Color.Gray;
+            BtnInsert.Enabled = true;
+            BtnInsert.BackColor = Color.RoyalBlue;
+        }
+
+        private void Navigate_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void BtnFirst_Click(object sender, EventArgs e)
+        {
+            if(Tr >= 0)
+            {
+                Rp = 0;
+                Navigation();
+                Enable();
+            }
+        }
+
+        private void BtnNext_Click(object sender, EventArgs e)
+        {
+            if(Rp < Tr)
+            {
+                Rp++;
+                Navigation();
+                Enable();
+            }
+        }
+
+        private void BtnPrev_Click(object sender, EventArgs e)
+        {
+            if(Rp > 0)
+            {
+                Rp--;
+                Navigation();
+                Enable();
+            }
+        }
+
+        private void BtnLast_Click(object sender, EventArgs e)
+        {
+            if(Tr >= 0)
+            {
+                Rp = Tr;
+                Navigation();
+                Enable();
+            }
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+            Disable();
+        }
+
+        
+
+        private void BtnInsert_Click(object sender, EventArgs e)
+        {
+            if (Unm.Text != "" && Cou.Text != "--Select Course--")
+            {
+                String InsertQuery = "Insert Into UserDetails Values('" + Unm.Text + "' , '" + Cou.Text + "')";
+                SqlDataAdapter DA = new SqlDataAdapter(InsertQuery, Con);
+                DataTable DT = new DataTable();
+                DA.Fill(DT);
+                Clear();
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("Enter Value In All Feilds !", "Crud&Navigation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            if (Unm.Text != "" && Cou.Text != "--Select Course--")
+            {
+                String UpdateQuery = "Update UserDetails Set Unm='"+Unm.Text+"' , Cou='"+Cou.Text+"' where id='"+id+"'";
+                SqlDataAdapter DA = new SqlDataAdapter(UpdateQuery, Con);
+                DataTable DT = new DataTable();
+                DA.Fill(DT);
+                Clear();
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("Enter Value In All Feilds !", "Crud&Navigation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult DR = MessageBox.Show("Are You Sure You Want To Delete ?","Crud&Navigation",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if(DR == DialogResult.Yes)
+            {
+                String UpdateQuery = "Delete From UserDetails where id='" + id + "'";
+                SqlDataAdapter DA = new SqlDataAdapter(UpdateQuery, Con);
+                DataTable DT = new DataTable();
+                DA.Fill(DT);
+                Clear();
+                LoadData();
+            }
+        }
+    }
+}
